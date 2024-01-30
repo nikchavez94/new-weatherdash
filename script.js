f9942c3e006b53277859b42c31171e0d3ab752ae
Nikki Vargas <angela94.chavez@gmail.com>
2024-01-29 22:21:19 -0600

    added apiKey

---
+++ script.js
@@ -0,0 +1,6 @@
+// Select the HTML element with class 'forecast-container'
+var daysElement = document.querySelector('.forecast-container'); 
+console.log(daysElement);
+
+// OpenWeatherMap API key
+const apiKey ='246778bea7563c3c26ec7a6a780eba81';
